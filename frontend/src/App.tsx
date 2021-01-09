8ac1a87fd1ffbd961649a35a5727342bc10dbd35
LinG <w93163red@gmail.com>
2021-01-09 17:31:04 -0500

    change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components'
-import { getPlayer } from './command/getdata';
+import { getState } from './command/getdata';
 
 function App() {
+  // state
+  const [gameState, setgameState] = useState({});
+
   const App = styled.div`
     min-height: 100vh;
   `;
 
-  getPlayer();
+  useEffect(() => {
+    async function getGameState() {
+      const gs = await getState();
+      setgameState(gs);
+    }
+    getGameState();
+  }, [])
+
 
   return (
     <App>
-      hello world
+      {JSON.stringify(gameState)}
     </App>
   );
 }
--- frontend/src/command/getdata.ts
+++ frontend/src/command/getdata.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:55555',
+    baseURL: 'http://192.168.50.116:55555',
     timeout: 1000,
 })
 
-export const getPlayer = async () => {
-    console.log(await instance.get('/player'));
+export const getState = async () => {
+    return await instance.get('/state');
 }
